3f56674d21eb8efa40e4d17fb0b5e7ba18d46a55
Jan Wöde <master@exclusiv-altis-life.de>
2021-09-15 00:02:01 +0200

    App kann wieder gebaut und veröffentlicht werden

---
+++ PK-Rechner.backup/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PK_Rechner
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            string[] geburtsdatum = Geburtstag.Eingabe();
+            int[] geburtstag = Geburtstag.Split(geburtsdatum);
+
+            int nachname = Nachname.Eingabe();
+
+            int _kwea = KWEA.Eingabe();
+            int[] kwea = KWEA.Split(_kwea);
+
+            Console.Write("Leitfadenzahl (eigl. immmer 1): ");
+            int lfd = Convert.ToInt32(Console.ReadLine());
+
+            int pruef = Rechnung(geburtstag, nachname, kwea, lfd);
+
+            string pk = $"{string.Join("", geburtsdatum)}-{Nachname.Buchstabe}-{_kwea}{lfd}{pruef}";
+            Console.WriteLine($"DEINE PERSONENKENNZIFFER LAUTET: {pk}");
+            Clipboard.SetText($"{pk}");
+            Console.WriteLine("\n\nDie PK wurde in die Zwischenablage kopiert!");
+            Console.ReadKey();
+        }
+
+        static int Rechnung(int[] geburtstag, int nachname, int[] kwea, int lfd)
+        {
+
+            geburtstag[0] *= 2;
+            geburtstag[1] *= 3;
+            geburtstag[2] *= 4;
+            geburtstag[3] *= 5;
+            geburtstag[4] *= 6;
+            geburtstag[5] *= 7;
+            kwea[0] *= 6;
+            kwea[1] *= 7;
+            kwea[2] *= 2;
+            lfd *= 3;
+
+            int sum = geburtstag[0] + geburtstag[1] + geburtstag[2] + geburtstag[3] + geburtstag[4] + geburtstag[5] + nachname + kwea[0] + kwea[1] + kwea[2] + lfd;
+            int mod = sum % 11;
+            int diff = 11 - mod;
+
+            switch (diff)
+            {
+                case 10:
+                    return 0;
+
+                case 11:
+                    return 1;
+
+                default:
+                    return diff;
+            }
+        }
+    }
+
+
+}
---
+++ PK-Rechner_WPF.backup/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PK_Rechner_WPF
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Nachname> nachnamen = new List<Nachname>();
+        List<KWEA> kweas = new List<KWEA>();
+
+        private Nachname nachname;
+        private int kwea;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AddVersionNumber();
+
+            nachnamen = Helper.InitializeChars(nachnamen);
+            Nachname.ItemsSource = nachnamen;
+
+            kweas = Helper.InitializeKWEAs(kweas);
+            KWEA.ItemsSource = kweas;
+        }
+
+        private void Berechnen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PKRechner pk = new PKRechner(Geburtsdatum.Text, nachname, kwea, int.Parse(Lfd.Text));
+                pk.PKBerechnen();
+                string pkennziff = pk.PK;
+
+                PK_lbl.Visibility = Visibility.Visible;
+                PK.Visibility = Visibility.Visible;
+                PK_bd.Visibility = Visibility.Visible;
+                PK.Text = pkennziff;
+                Clipboard.SetText($"{pkennziff}");
+                PK_Cpy_lbl.Visibility = Visibility.Visible;
+            }
+            catch
+            {
+                MessageBox.Show("Bitte geben Sie Daten ein, die vom Programm verarbeitet werden können!\n\nBei Fragen wenden Sie sich bitte an den Hersteller.", "Bitte korrekte Daten eingeben", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void AddVersionNumber()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            this.Title += $" v.{versionInfo.FileVersion}";
+            Autor.Content = versionInfo.LegalCopyright;
+        }
+
+        private void Nachname_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Nachname item = Nachname.SelectedItem as Nachname;
+            nachname = item;
+        }
+
+        private void KWEA_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            KWEA item = KWEA.SelectedItem as KWEA;
+            kwea = item.Nummer;
+        }
+    }
+}
